803fed734793de4d25753dd0e815da76b0f38220
Lukas Palmer <lnpalmer@continuum>
2018-04-24 11:02:49 -0400

    moving to Python, phew

--- src/main.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <primary.h>
-using namespace std;
-
-int main() {
-  PrimaryNetwork* primaryNetwork = new PrimaryNetwork(400, 10, 10);
-
-  cout << sizeof(long long) << endl;
-}
